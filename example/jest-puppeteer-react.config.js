e3f17621236e337c37e4dd41774add00b1c0ae4e
Stefan Schult <stefan@schult.mobi>
2018-06-28 09:38:46 +0200

    added some more peer dependencies

--- example/jest-puppeteer-react.config.js
+++ example/jest-puppeteer-react.config.js
@@ -11,6 +11,7 @@ module.exports = {
         aliasObject
     ) {
         return {
+            mode: 'development',
             entry: { test: entryFiles },
             devtool: 'eval-source-map',
             output: {
