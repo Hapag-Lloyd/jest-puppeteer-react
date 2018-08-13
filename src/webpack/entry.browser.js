917aa8bd52053341d3c7732739517568df0efbb7
Stefan Schult <stefan@schult.mobi>
2018-08-13 18:25:48 +0200

    added test preview on same page as the overview

--- src/webpack/entry.browser.js
+++ src/webpack/entry.browser.js
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 const search = window.location.search;
+const urlParams = new URLSearchParams(window.location.search);
+let currentTest = urlParams.get('testPreview') || urlParams.get('test');
 
-try {
+if (urlParams.has('test')) {
     const testName = decodeURIComponent(search.match(/\?test=([^&]+)/i)[1]);
 
     const component =
-        window.__tests[testName] ||
+        (window.__tests[testName] || {}).reactNode ||
         React.createElement(
             'div',
             null,
@@ -15,21 +17,61 @@ try {
         );
 
     ReactDOM.render(component, document.getElementById('main'));
-} catch (e) {
+} else {
+    const wrapper = document.createElement('div');
+    wrapper.style.setProperty('display', 'grid');
+    wrapper.style.setProperty('grid-template-columns', '200px auto');
+    wrapper.style.setProperty('grid-gap', '1em');
+
+    // frame to render the test preview
+    const container = document.createElement('div');
+    container.style.setProperty('position', 'relative');
+    container.style.setProperty('overflow', 'auto');
+
     // show a list of all tests, only when search is empty
-    const fragment = document.createDocumentFragment();
+    const list = document.createElement('ul');
+    list.style.setProperty('margin', '0');
+    list.style.setProperty('padding', '0');
+    list.style.setProperty('display', 'grid');
+    list.style.setProperty('grid-gap', '.32em');
+
     Object.keys(__tests)
         .sort((a, b) => a.localeCompare(b))
         .map(k => {
             const a = document.createElement('a');
-            a.href = `${document.location.protocol}//${
+            a.style.setProperty('display', 'block');
+
+            const url = `${document.location.protocol}//${
                 document.location.hostname
-            }:${document.location.port}?test=${encodeURIComponent(k)}`;
+            }:${document.location.port}?testPreview=${encodeURIComponent(k)}`;
+
+            a.href = url;
+
+            a.onclick = e => {
+                e.stopPropagation();
+                window.history.pushState({}, k, url);
+                ReactDOM.render(__tests[k].reactNode, container);
+                return false;
+            };
+
+            a.onmouseenter = e => {
+                e.target.style.setProperty('background-color', '#ddd');
+            };
+            a.onmouseleave = e => {
+                e.target.style.removeProperty('background-color');
+            };
             a.text = k;
-            return a;
+
+            if (k === currentTest) {
+                ReactDOM.render(__tests[k].reactNode, container);
+            }
+
+            const item = document.createElement('li');
+            item.appendChild(a);
+            return item;
         })
-        .forEach(k => {
-            fragment.appendChild(k), fragment.appendChild(document.createElement('br'));
-        });
-    document.body.appendChild(fragment);
+        .forEach(k => list.appendChild(k));
+    wrapper.appendChild(list);
+    wrapper.appendChild(container);
+    document.body.appendChild(wrapper);
 }
