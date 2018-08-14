55e112c817c465edca0499bea9418785203dfd3a
Stefan Schult <Schubidu@users.noreply.github.com>
2018-08-14 09:39:26 +0200

    added test preview on same page as the overview (#5)

--- src/webpack/render.browser.js
+++ src/webpack/render.browser.js
@@ -3,5 +3,5 @@ import React from 'react';
 export const render = async (reactNode, options) => {
     const testName = window.__path.join(' ');
     // console.log('browser renderer', reactNode, testName);
-    window.__tests[testName] = reactNode;
+    window.__tests[testName] = { reactNode, path: [...window.__path] };
 };
