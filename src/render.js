01276071c81aa40eadca07eaa17585d43132006c
Aleksei Androsov <aandrosov@yandex-team.ru>
2019-04-30 15:23:49 +0300

    Add renderOptions.dumpConsole
    This option dump all console.* calls from puppeteer. Useful for debug

--- src/render.js
+++ src/render.js
@@ -20,6 +20,22 @@ async function render(reactNode, options) {
         pageConfig.waitUntil = opts.waitUntil;
     }
 
+    // jest-puppeteer reuses page (browser tab) beetween tests
+    // `__jestReactPuppeteerEventsSubscription` flag needs to avoid subscription duplication
+    if (opts.dumpConsole && !page.__jestReactPuppeteerEventsSubscription) {
+        page.on('console', msg => {
+            console.log(`event "console" from "${currentTestName}"`);
+            console[msg.type()](msg.text());
+        });
+        page.on('error', msg => {
+            console.error(`event "error" from "${currentTestName}"`, msg);
+        });
+        page.on('pageerror', msg => {
+            console.error(`event "pageerror" from "${currentTestName}"`, msg);
+        });
+        page.__jestReactPuppeteerEventsSubscription = true;
+    }
+
     debug('page.goto ' + url);
     await page.goto(url, pageConfig);
 
