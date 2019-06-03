4d5828f1cdce5fec3e77346512b3358a0eb579c6
Aleksei Androsov <aandrosov@yandex-team.ru>
2019-06-03 12:45:27 +0300

    Test name collision detection
    This is very frustrating situation when I have 2 files without global describe() and same it('testname')

--- src/webpack/render.browser.js
+++ src/webpack/render.browser.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export const render = async (reactNode, options) => {
     const testName = window.__path.join(' ');
+
+    if (testName in window.__tests) {
+        throw new Error(
+            `Test name collision detected for "${testName}". Please use describe() or rename tests.`
+        );
+    }
+
     // console.log('browser renderer', reactNode, testName);
     window.__tests[testName] = Object.assign({}, options, {
         reactNode,
