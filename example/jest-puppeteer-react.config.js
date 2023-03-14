5a84f7c3192d61a012a0ff3962741c9a44ef8299
rosczja <rosczja@hlag.com>
2023-03-14 08:38:22 +0100

    Upgrade dependencies

--- example/jest-puppeteer-react.config.js
+++ example/jest-puppeteer-react.config.js
@@ -74,4 +74,26 @@ module.exports = {
     useHttps: true,
     useDocker: true,
     dockerHost: dockerHost(),
+    dockerEntrypoint: '""', // overwrites the default entrypoint of the image
+    dockerRunOptions: '--shm-size=1g',
+    dockerCommand:
+        'google-chrome ' +
+        '--no-sandbox ' +
+        '--disable-background-networking ' +
+        '--disable-default-apps ' +
+        '--disable-extensions ' +
+        '--disable-sync ' +
+        '--disable-translate ' +
+        '--headless ' +
+        '--hide-scrollbars ' +
+        '--metrics-recording-only ' +
+        '--mute-audio ' +
+        '--no-first-run ' +
+        '--safebrowsing-disable-auto-update ' +
+        '--ignore-certificate-errors ' +
+        '--ignore-ssl-errors ' +
+        '--ignore-certificate-errors-spki-list ' +
+        '--user-data-dir=/tmp ' +
+        '--remote-debugging-port=9222 ' +
+        '--remote-debugging-address=0.0.0.0',
 };
